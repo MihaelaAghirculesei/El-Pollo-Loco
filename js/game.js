6760a850871cc4038e65caee0082406a9ee53728
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-02-17 12:46:13 +0100

    Added headline

--- js/game.js
+++ js/game.js
@@ -1,12 +1,12 @@
 let canvas;
 let ctx;
-let character = new MovableObject();
+let world;
 
 function init() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d"); // ctx ist variable Name
-
-  console.log("My Character is");
+world = new World(canvas)
+//  console.log('My Character is', world['character']);
 
   //PROBE
   // character.src = '../img_pollo_locco/img/2_character_pepe/1_idle/idle/I-1.png';
