b5e755598772033449a0bc332b09449b252017de
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-02-19 23:31:08 +0100

    Added headline

--- models/world.class.js
+++ models/world.class.js
@@ -1,27 +1,29 @@
 class World {
   character = new Character();
-  enemies = [
-    new Chicken(), 
-    new Chicken(), 
-    new Chicken(),
-  ];
-  clouds = [
-    new Cloud()
-  ];
+  enemies = [new Chicken(), new Chicken(), new Chicken()];
+  clouds = [new Cloud()];
   backgroundObjekts = [
-    new BackgroundObjekt('img_pollo_locco/img/5_background/layers/air.png', 0,),
-    new BackgroundObjekt('img_pollo_locco/img/5_background/layers/3_third_layer/1.png', 0,),
-    new BackgroundObjekt('img_pollo_locco/img/5_background/layers/2_second_layer/1.png', 0),
-    new BackgroundObjekt('img_pollo_locco/img/5_background/layers/1_first_layer/1.png', 0), 
-];
+    new BackgroundObjekt("img_pollo_locco/img/5_background/layers/air.png", 0),
+    new BackgroundObjekt(
+      "img_pollo_locco/img/5_background/layers/3_third_layer/1.png",
+      0
+    ),
+    new BackgroundObjekt(
+      "img_pollo_locco/img/5_background/layers/2_second_layer/1.png",
+      0
+    ),
+    new BackgroundObjekt(
+      "img_pollo_locco/img/5_background/layers/1_first_layer/1.png",
+      0
+    ),
+  ];
   canvas;
   ctx;
   keyboard;
 
-
   constructor(canvas, keyboard) {
     this.canvas = canvas;
-    this.ctx = this.canvas.getContext('2d'); 
+    this.ctx = this.canvas.getContext("2d");
     this.keyboard = keyboard;
     this.draw();
     this.setWorld();
@@ -31,7 +33,6 @@ class World {
     this.character.world = this;
   }
 
-
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
@@ -40,24 +41,21 @@ class World {
     this.addToMap(this.character);
     this.addObjectsToMap(this.clouds);
     this.addObjectsToMap(this.enemies);
-   
 
     // Draw wird immer wieder aufgerufen
     let self = this;
-    requestAnimationFrame(function() {
+    requestAnimationFrame(function () {
       self.draw();
     });
   }
 
   addObjectsToMap(objects) {
-    objects.forEach(o => {
+    objects.forEach((o) => {
       this.addToMap(o);
-});
+    });
   }
 
   addToMap(mo) {
     this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }
-
-
