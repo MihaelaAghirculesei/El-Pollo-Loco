1e99c8e131552257fa04140556ab4638615dc4e2
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-02-20 12:03:44 +0100

    Added headline

--- models/character.class.js
+++ models/character.class.js
@@ -11,30 +11,33 @@ class Character extends MovableObject {
     "img_pollo_locco/img/2_character_pepe/2_walk/W-26.png",
   ];
   world;
-
-constructor(){
-    super().loadImage('img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
+  constructor() {
+    super().loadImage("img_pollo_locco/img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
-
     this.animate();
   }
-
   animate() {
-
     setInterval(() => {
-        if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-            this.x += this.speed;
-             let i = this.currentImage % this.IMAGES_WALKING.length; //% =den rest und erhöht der index ab 7, let i = 6; 
-              // Walk animation i = 0,1,2,3,4,5,0,1,2,3,4,5,0
-             let path = this.IMAGES_WALKING[i];
-             this.img = this.imageCache[path];
-             this.currentImage++;
-    }
-    },50);
-}
-
-    jump(){
-
+      if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+        this.otherDirection = false;
+      }
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+        this.otherDirection = true;
+      }
+    }, 1000 / 60);
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        // Walk animation
+        let i = this.currentImage % this.IMAGES_WALKING.length; //% =den rest und erhöht der index ab 7, let i = 6;
+        // i = 0,1,2,3,4,5,0,1,2,3,4,5,0
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 50);
+  }
     update() {
       if (this.world.keyboard.RIGHT) {
         this.x += this.speed;
@@ -46,7 +49,5 @@ constructor(){
       }
     }
   
-
-
-}
+  jump() {}
 }
