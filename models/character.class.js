344d6aba22aa0c88aa9077c5ed848f2dc0f2ebf3
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-02-20 08:57:54 +0100

    Added headline

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
 
 height = 280;
 y = 160;
+speed = 10;
 IMAGES_WALKING = [
     'img_pollo_locco/img/2_character_pepe/2_walk/W-21.png',
     'img_pollo_locco/img/2_character_pepe/2_walk/W-22.png',
@@ -20,15 +21,28 @@ constructor(){
 }
 
 animate() {
+
     setInterval(() => {
         if(this.world.keyboard.RIGHT) {
-        let i = this.currentImage % this.IMAGES_WALKING.length; //% =den rest und erhöht der index ab 7, let i = 6; 
-        // i = 0,1,2,3,4,5,0,1,2,3,4,5,0
-        let path = this.IMAGES_WALKING[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
+            this.x += this.speed;
+        }
+
+        if(this.world.keyboard.LEFT) {
+            this.x -= this.speed;
+        }
+    }, 1000 / 60);
+
+
+    setInterval(() => {
+        if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+            this.x += this.speed;
+             let i = this.currentImage % this.IMAGES_WALKING.length; //% =den rest und erhöht der index ab 7, let i = 6; 
+              // Walk animation i = 0,1,2,3,4,5,0,1,2,3,4,5,0
+             let path = this.IMAGES_WALKING[i];
+             this.img = this.imageCache[path];
+             this.currentImage++;
     }
-    },1000);
+    },50);
 }
 
     jump(){
