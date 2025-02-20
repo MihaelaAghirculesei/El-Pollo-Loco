028b6f1f2fafc2f4b53c288303e86c613897b857
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-02-20 11:38:03 +0100

    Added headline

--- models/character.class.js
+++ models/character.class.js
@@ -12,19 +12,6 @@ class Character extends MovableObject {
   ];
   world;
 
-height = 280;
-y = 160;
-speed = 10;
-IMAGES_WALKING = [
-    'img_pollo_locco/img/2_character_pepe/2_walk/W-21.png',
-    'img_pollo_locco/img/2_character_pepe/2_walk/W-22.png',
-    'img_pollo_locco/img/2_character_pepe/2_walk/W-23.png',
-    'img_pollo_locco/img/2_character_pepe/2_walk/W-24.png',
-    'img_pollo_locco/img/2_character_pepe/2_walk/W-25.png',
-    'img_pollo_locco/img/2_character_pepe/2_walk/W-26.png'
-];
-world;
-
 constructor(){
     super().loadImage('img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
     this.loadImages(this.IMAGES_WALKING);
@@ -33,31 +20,6 @@ constructor(){
   }
 
   animate() {
-    // setInterval(() => {
-    //   if (this.world.keyboard.RIGHT) {
-    //     this.x += this.speed;
-    //     this.otherDirection = false;
-    //   }
-    //   if (this.world.keyboard.LEFT) {
-    //     this.x -= this.speed;
-    //     this.otherDirection = true;
-    //   }
-    // }, 1000 / 60);
-
-animate() {
-
-    setInterval(() => {
-        if(this.world.keyboard.RIGHT) {
-            this.x += this.speed;
-            this.otherDirection = false;
-        }
-
-        if(this.world.keyboard.LEFT) {
-            this.x -= this.speed;
-            this.otherDirection = true;
-        }
-    }, 1000 / 60);
-
 
     setInterval(() => {
         if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
@@ -85,5 +47,6 @@ animate() {
     }
   
 
-  jump() {}
+
+}
 }
