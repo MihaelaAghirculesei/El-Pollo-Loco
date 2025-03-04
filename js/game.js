297b5c43cb7e8b281aad7f4e719d182c125f528f
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-03-04 12:45:02 +0100

    Added headline

--- js/game.js
+++ js/game.js
@@ -3,51 +3,83 @@ let world;
 let keyboard = new Keyboard();
 
 function init() {
-  playBackgroundMusic()
-  canvas = document.getElementById("canvas");
-  world = new World(canvas, keyboard);
+    playBackgroundMusic()
+    canvas = document.getElementById("canvas");
+    world = new World(canvas, keyboard);
 
-  console.log('My Character is', world.character);
+    console.log('My Character is', world.character);
 }
 
 window.addEventListener("keydown", (e) => {
-  if (e.keyCode == 39) {
-      keyboard.RIGHT = true;
-  }
-  if (e.keyCode == 37) {
-      keyboard.LEFT = true;
-  }
-  if (e.keyCode == 38) {
-      keyboard.UP = true;
-  }
-  if (e.keyCode == 40) {
-      keyboard.DOWN = true;
-  }
-  if (e.keyCode == 32) {
-      keyboard.SPACE = true;
-  }
-  if (e.keyCode == 68) {
-    keyboard.D = true;
-}
-}); 
+    if (e.keyCode == 39) {
+        keyboard.RIGHT = true;
+    }
+    if (e.keyCode == 37) {
+        keyboard.LEFT = true;
+    }
+    if (e.keyCode == 38) {
+        keyboard.UP = true;
+    }
+    if (e.keyCode == 40) {
+        keyboard.DOWN = true;
+    }
+    if (e.keyCode == 32) {
+        keyboard.SPACE = true;
+    }
+    if (e.keyCode == 68) {
+        keyboard.D = true;
+    }
+});
 
 window.addEventListener("keyup", (e) => {
-  if (e.keyCode == 39) {
-      keyboard.RIGHT = false;
-  }
-  if (e.keyCode == 37) {
-      keyboard.LEFT = false;
-  }
-  if (e.keyCode == 38) {
-      keyboard.UP = false;
-  }
-  if (e.keyCode == 40) {
-      keyboard.DOWN = false;
-  }
-  if (e.keyCode == 32) {
-      keyboard.SPACE = false;
-  }
-  if (e.keyCode == 68) {
-    keyboard.D = false;
-}
+    if (e.keyCode == 39) {
+        keyboard.RIGHT = false;
+    }
+    if (e.keyCode == 37) {
+        keyboard.LEFT = false;
+    }
+    if (e.keyCode == 38) {
+        keyboard.UP = false;
+    }
+    if (e.keyCode == 40) {
+        keyboard.DOWN = false;
+    }
+    if (e.keyCode == 32) {
+        keyboard.SPACE = false;
+    }
+    if (e.keyCode == 68) {
+        keyboard.D = false;
+    }
 });
+
+function startGame() {
+    document.getElementById("startScreen").style.display = "none";
+    document.getElementById('content').style.display = 'block';
+    setTimeout(() => {
+        init();
+    }, 300);
+}
+
+function openControls() {
+    document.getElementById("controlsScreen").style.display = "flex";
+}
+
+function closeControls() {
+    document.getElementById("controlsScreen").style.display = "none";
+}
+
+function openStory() {
+    document.getElementById("storyScreen").style.display = "flex";
+}
+
+function closeStory() {
+    document.getElementById("storyScreen").style.display = "none";
+}
+
+function openSettings() {
+    document.getElementById("settingsScreen").style.display = "flex";
+}
+
+function closeSettings() {
+    document.getElementById("settingsScreen").style.display = "none";
+}
