9df5e9ed0f54afec1391962ac5472612a161c116
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-06-19 12:08:25 +0200

    Refactor(status-bar-heart-endboss): split logic into single-responsibility functions

--- models/status-bar-heart-endboss.class.js
+++ models/status-bar-heart-endboss.class.js
@@ -5,42 +5,58 @@ class StatusBarHeartEndboss extends DrawableObject {
     "img_pollo_locco/img/7_statusbars/2_statusbar_endboss/40.png",
     "img_pollo_locco/img/7_statusbars/2_statusbar_endboss/60.png",
     "img_pollo_locco/img/7_statusbars/2_statusbar_endboss/80.png",
-    "img_pollo_locco/img/7_statusbars/2_statusbar_endboss/100.png",
+    "img_pollo_locco/img/7_statusbars/2_statusbar_endboss/100.png"
   ];
+
   percentage = 100;
+
   constructor(endboss) {
     super();
     this.endboss = endboss;
+    this.setupStatusBar();
     this.loadImages(this.IMAGES);
-    this.setPosition();
     this.setPercentage(this.endboss.getHealthPercent());
   }
-  setPosition() {
+
+  setupStatusBar() {
     this.x = 495;
     this.y = 5;
     this.width = 200;
     this.height = 67;
   }
+
   setPercentage(percentage) {
     this.percentage = this.clampPercentage(percentage);
-    this.img = this.imageCache[this.IMAGES[this.getImageIndex()]];
+    this.updateStatusBarImage();
   }
+
   clampPercentage(percentage) {
     return Math.max(0, Math.min(percentage, 100));
   }
+
+  updateStatusBarImage() {
+    const imageIndex = this.getImageIndex();
+    this.img = this.imageCache[this.IMAGES[imageIndex]];
+  }
+
   getImageIndex() {
-    if (this.percentage == 100) return 5;
+    if (this.percentage === 100) return 5;
     if (this.percentage >= 80) return 4;
     if (this.percentage >= 60) return 3;
     if (this.percentage >= 40) return 2;
     if (this.percentage >= 20) return 1;
     return 0;
   }
+
   draw(ctx) {
-    this.x = ctx.canvas.width - this.width - 20;
+    this.updateCanvasPosition(ctx);
     if (this.img) {
       ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
+  }
+
+  updateCanvasPosition(ctx) {
+    this.x = ctx.canvas.width - this.width - 20;
   }
 
   updateHealth() {
