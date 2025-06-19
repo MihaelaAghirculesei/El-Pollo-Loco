542eec466078b8c4951303365e708d5f59c1f49f
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-06-19 11:15:45 +0200

    Refactor(status-bar-bottle): split logic into single-responsibility functions

--- models/status-bar-bottle.class.js
+++ models/status-bar-bottle.class.js
@@ -6,25 +6,45 @@ class StatusBarBottle extends DrawableObject {
     "img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/40.png",
     "img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/60.png",
     "img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/80.png",
-    "img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/100.png",
+    "img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/100.png"
   ];
 
   bottlesCount = 0;
 
   constructor() {
     super();
+    this.setupStatusBar();
     this.loadImages(StatusBarBottle.IMAGES);
+    this.setBottlesCount(0);
+  }
+
+  setupStatusBar() {
     this.x = 40;
     this.y = 40;
     this.width = 200;
     this.height = 60;
-    this.setBottlesCount(0);
   }
 
   setBottlesCount(count) {
-    this.bottlesCount = Math.max(0, Math.min(count, StatusBarBottle.MAX_BOTTLES));
-    const percentage = this.bottlesCount / StatusBarBottle.MAX_BOTTLES;
-    const imageIndex = Math.floor(percentage * (StatusBarBottle.IMAGES.length - 1));
+    this.bottlesCount = this.clampBottleCount(count);
+    this.updateStatusBarImage();
+  }
+
+  clampBottleCount(count) {
+    return Math.max(0, Math.min(count, StatusBarBottle.MAX_BOTTLES));
+  }
+
+  updateStatusBarImage() {
+    const percentage = this.calculatePercentage();
+    const imageIndex = this.getImageIndex(percentage);
     this.img = this.imageCache[StatusBarBottle.IMAGES[imageIndex]];
+  }
+
+  calculatePercentage() {
+    return this.bottlesCount / StatusBarBottle.MAX_BOTTLES;
+  }
+
+  getImageIndex(percentage) {
+    return Math.floor(percentage * (StatusBarBottle.IMAGES.length - 1));
   }
 }
